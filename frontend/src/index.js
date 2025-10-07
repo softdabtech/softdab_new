364dd5507bb5ddb63c45613232ed3a5fcb9a08e8
Alex <son_alex@ukr.net>
2025-10-08 00:30:45 +0300

    many changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: process.env.REACT_APP_POSTHOG_HOST || "https://us.i.posthog.com",
+  // defaults: "2025-05-24", // опционально, можно убрать
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <PostHogProvider apiKey={process.env.REACT_APP_POSTHOG_KEY} options={options}>
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
 );
