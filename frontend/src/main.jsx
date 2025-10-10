ae540d5a41c1720e296c19a88d0eb2ca60c567d9
Alex <son_alex@ukr.net>
2025-10-10 23:44:56 +0300

    main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,10 @@ const root = createRoot(container);
 const POSTHOG_KEY = import.meta.env.VITE_POSTHOG_KEY;
 const POSTHOG_HOST = import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com';
 
+if (POSTHOG_KEY) {
+  posthog.init(POSTHOG_KEY, { api_host: POSTHOG_HOST });
+}
+
 const app = (
   <React.StrictMode>
     <BrowserRouter>
