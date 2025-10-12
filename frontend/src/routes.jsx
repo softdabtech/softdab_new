63f467850ed4ac43ff62109ce98a2dd7afde5a1e
Alex <son_alex@ukr.net>
2025-10-12 23:49:16 +0300

    debug: Isolate AboutPage route

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,24 +1,10 @@
+// frontend/src/routes.jsx (ВЕРСИЯ ДЛЯ ТЕСТА)
 import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-// Lazy-loaded components
-const HomePage = React.lazy(() => import('./pages/HomePage'));
-const CaseStudiesPage = React.lazy(() => import('./pages/CaseStudiesPage'));
-const CaseDetailPage = React.lazy(() => import('./pages/CaseDetailPage'));
+// Оставляем только самые необходимые импорты для теста
 const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
-
-// Company pages
 const AboutPage = React.lazy(() => import('./pages/company/AboutPage'));
-const ContactPage = React.lazy(() => import('./pages/company/ContactPage'));
-const CareersPage = React.lazy(() => import('./pages/company/CareersPage'));
-
-// Services pages
-const ServicesPage = React.lazy(() => import('./pages/services/ServicesPage'));
-const CustomDevelopmentPage = React.lazy(() => import('./pages/services/CustomDevelopmentPage'));
-const DedicatedTeamPage = React.lazy(() => import('./pages/services/DedicatedTeamPage'));
-
-// Industries pages
-const IndustriesPage = React.lazy(() => import('./pages/industries/IndustriesPage'));
 
 // Импорт компонента загрузки
 import { LoadingSkeleton } from './components/ui/loading';
@@ -27,24 +13,14 @@ export const AppRoutes = () => {
   return (
     <Suspense fallback={<LoadingSkeleton />}>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        
-        {/* Case Studies */}
-        <Route path="/case-studies" element={<CaseStudiesPage />} />
-        <Route path="/case-studies/:slug" element={<CaseDetailPage />} />
-        
-        {/* Company */}
+        {/* 
+          Для теста мы направляем главную страницу (/) на AboutPage.
+          Это упростит проверку.
+        */}
+        <Route path="/" element={<AboutPage />} />
         <Route path="/company/about" element={<AboutPage />} />
-        <Route path="/company/contact" element={<ContactPage />} />
-        <Route path="/company/careers" element={<CareersPage />} />
         
-        {/* Services */}
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/services/custom-development" element={<CustomDevelopmentPage />} />
-        <Route path="/services/dedicated-team" element={<DedicatedTeamPage />} />
-        
-        {/* Industries */}
-        <Route path="/industries" element={<IndustriesPage />} />
+        {/* Все остальные роуты временно отключены */}
         
         {/* 404 */}
         <Route path="*" element={<NotFoundPage />} />
